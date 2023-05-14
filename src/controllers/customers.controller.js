7d0312d28eef45f47f97da964dfc47306d4c53e2
Eduardo-JP-Ferreira <eduardopires2014@gmail.com>
2023-05-13 22:04:29 -0300

    Upgrade Listar Clientes

--- src/controllers/customers.controller.js
+++ src/controllers/customers.controller.js
@@ -1,10 +1,23 @@
+import dayjs from "dayjs"
 import { db } from "../database/connection.js"
 
 export async function getCustomers(req, res) {
     try {
         const customers = await db.query(`SELECT * FROM customers;`)
-        console.table(customers .rows)
-        res.send(customers .rows)
+        const arr =[]
+        for(let i=0;i<customers.rows.length;i++){
+            let data = dayjs(customers.rows[i].birthday).format('YYYY-MM-DD')
+            const customerObject = {
+                id: customers.rows[i].id,
+                name: customers.rows[i].name,
+                phone: customers.rows[i].phone,
+                cpf: customers.rows[i].cpf,
+                birthday: data
+            }
+            arr.push(customerObject)
+        }
+        console.table(customers.rows)
+        res.send(arr)
     } catch (err) {
         res.status(500).send(err.message)
     }
@@ -23,8 +36,9 @@ export async function postCustomers(req, res) {
             INSERT INTO customers (name, phone, cpf, birthday) 
             VALUES ($1, $2, $3, $4);`, [name,phone,cpf,birthday])
         }
-        console.log(birthday)
-        res.sendStatus(201)
+        // console.log(birthday)
+        // res.sendStatus(201)
+        res.status(201).send(birthday)
     } catch (err) {
         res.status(500).send(err.message)
     }
--- src/schemas/validate.schema.js
+++ src/schemas/validate.schema.js
@@ -11,5 +11,5 @@ export const customerObject = joi.object({
     name: joi.string().required(),
     phone: joi.string().regex(/^\d+$/).min(10).max(11).required(),
     cpf: joi.string().regex(/^\d+$/).length(11).required(),
-    birthday: joi.date().iso().required()
+    birthday: joi.date().required()
   })
